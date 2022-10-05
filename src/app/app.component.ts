8966959d627fe1ffe0a9e782e3afe33b9491e51a
makaempffer <makaempffer@duocuc.cl>
2022-10-04 22:35:14 -0300

    ahorasi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,7 @@ export class AppComponent {
   public appPages = [
     { title: 'Home', url: 'home', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-<<<<<<< HEAD
     { title: 'Cerrar Sesión', url: '', icon: 'log-out' },
-=======
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'About', url: '/about', icon: 'warning' },
->>>>>>> 6b917cb03559ac47fe90d6d050ff832aedfb1855
   ];
   constructor() {}
 }
