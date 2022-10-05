22cc9175596db14df3f13faabf208b56c77067b4
Francis-GG <frai.gonzalez@duocuc.cl>
2022-10-04 22:37:45 -0300

    About sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
     { title: 'Home', url: 'home', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
     { title: 'Cerrar Sesión', url: '', icon: 'log-out' },
+    { title: 'About', url: '/about', icon: 'warning' },
   ];
   constructor() {}
 }
